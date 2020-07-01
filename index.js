670520d003a457b311ca4a76eedfe5b2ff5e45fe
whzyustc <whzyzyz@outlook.com>
2020-07-01 11:35:07 +0800

    bug fixed#1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const bodyParser=require('body-parser');
 const app = express();
-const moment= require('moment');
 const port=3000;
+const bodyParser=require('body-parser');
+const moment= require('moment');
 
-var mysql=require('mysql');
-var connection = mysql.createConnection({
+const mysql=require('mysql');
+const connection = mysql.createConnection({
     host:'localhost',
     user:'oxygensensor',
     password:'\$Ano2012',
@@ -13,29 +13,26 @@ var connection = mysql.createConnection({
 })
 
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 console.log(__dirname);
 app.use('/',express.static(__dirname+'/dist'))
-/*app.get('/',function(req,res){
-    res.send('Hello World!');
-
-    //console.log(req);
-});*/
+var lastid=0;
 
 app.get('/last60s',function(req,res){
-    connection.query(`select * from test2 limit 100 `,function(err,results,fields){
+    connection.query(`select * from (select * from test2 order by date desc limit 100) as tmp order by id`,function(err,results,fields){
         if (err) throw err;
-        console.log(results[0]);
+        console.log(results[results.length-1]);
         res.send(results);
     });
 })
 
 app.get('/datafromdate',function(req,res){
-    connection.query(`select * from test2 limit 100 `,function(err,results,fields){
+    console.log(req.query);
+    connection.query(`select * from test2 where id>${lastid} order by id `,function(err,results,fields){
         if (err) throw err;
-        console.log(results[0]);
-        res.send(results[0]);
+        res.send(results);
     });
 })
 
@@ -45,7 +42,7 @@ app.post('/post',function(req,res){
     connection.query(`insert into test2
         (date,oxygenValue)
         values
-        ("${current_time}",${req.body.hello.substring(0,3)})
+        ("${current_time}",${req.body.message})
         ;`);
     res.send("post success");
     console.log(req.body);
@@ -58,33 +55,3 @@ app.listen(port,function(){
 });
 
 
-// var net = require('net');
- 
-// var HOST = '0.0.0.0';
-// var PORT = 3000;
- 
-// // 创建一个TCP服务器实例，调用listen函数开始监听指定端口
-// // 传入net.createServer()的回调函数将作为”connection“事件的处理函数
-// // 在每一个“connection”事件中，该回调函数接收到的socket对象是唯一的
-// net.createServer(function(sock) {
- 
-//     // 我们获得一个连接 - 该连接自动关联一个socket对象
-//     console.log('CONNECTED: ' +
-//         sock.remoteAddress + ':' + sock.remotePort);
- 
-//     // 为这个socket实例添加一个"data"事件处理函数
-//     sock.on('data', function(data) {
-//         console.log('DATA ' + sock.remoteAddress + ': ' + data);
-//         // 回发该数据，客户端将收到来自服务端的数据
-//         sock.write('You said "' + data + '"');
-//     });
- 
-//     // 为这个socket实例添加一个"close"事件处理函数
-//     sock.on('close', function(data) {
-//         console.log('CLOSED: ' +
-//             sock.remoteAddress + ' ' + sock.remotePort);
-//     });
- 
-// }).listen(PORT, HOST);
- 
-// console.log('Server listening on ' + HOST +':'+ PORT);
